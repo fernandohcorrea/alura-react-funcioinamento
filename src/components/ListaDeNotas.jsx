a7258c2ab86b715cb00de381cb83d9ebcf1927b4
Fernando Henrique Correa <fernando.correa@kymo.it>
2020-11-23 21:00:24 -0300

    2 - 23/11 - Componentizando o sistema

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,24 +1,34 @@
 import React, { Component } from "react";
-import CardNota from './CardNota'
+import CardNota from "./CardNota";
 
 class ListaDeNotas extends Component {
+  state = {
+    notas: [
+      {
+        title: "asd",
+        text: "asdasdasd",
+      },
+      {
+        title: "qwe",
+        text: "qweqweqwe",
+      },
+    ],
+  };
 
   render() {
     return (
       <ul className="list">
-        <li>
-          <CardNota/>
-        </li>
-        <li>
-          <CardNota/>
-        </li>
-        <li>
-          <CardNota/>
-        </li>
+        {this.state.notas.map((NoteData) => {
+          return (
+            <li>
+              <div>{NoteData.title}</div>
+              <CardNota />
+            </li>
+          );
+        })}
       </ul>
     );
   }
-
 }
 
-export default ListaDeNotas
+export default ListaDeNotas;
